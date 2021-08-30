93b5b41f9f5088e9e9639324258a0b0c421f36ad
Adão Benites <adaobjr@gmail.com>
2021-08-30 18:54:53 -0400

    feat: ✨ added Routers in App.js  / Index.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
+import { Switch, Route } from 'react-router-dom';
+import Home from './common/pages/Home';
+// import Favorited from './common/pages/Favorited';
+import Cart from './common/pages/Cart';
+
 function App() {
   return (
-    <div>
-      Aplicação rodando
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      {/* <Route exact path="/favoritos" component={Favorited} /> */}
+      <Route exact path="/carrinho" component={Cart} />
+    </Switch>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter } from 'react-router-dom';
+import Provider from './context/Provider';
 import './index.css';
 import App from './App';
+import store from './context/store';
 
 ReactDOM.render(
-  <App />,
+  <HashRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </HashRouter>,
   document.getElementById('root'),
 );
