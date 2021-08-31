5a01d99504f3339445c6dfc02ff3cf4a5542cb25
Adão Benites <adaobjr@gmail.com>
2021-08-31 09:31:22 -0400

    style: :lipstick: added initial styles in Home

--- src/common/pages/Home.jsx
+++ src/common/pages/Home.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+
 import Header from '../components/Header';
+import img5 from '../../files/images/image_5.png';
+import img6 from '../../files/images/image_6.png';
 
 export default function Home() {
   const renderHome = () => (
-    /* <!--========== HEADER ==========--> */
-    <Header />
+    <>
+      {/* <!--========== HEADER ==========--> */}
+      <Header />
+
+      {/* <!--========== IMAGES ==========--> */}
+      <div className="topImages">
+        <div>
+          <p className="titleTopImg">
+            ÍRIS
+            <br />
+            VERÃO
+            <br />
+            2022
+          </p>
+          <img src={img5} alt="imagem destaque 1" />
+        </div>
+        <div>
+          <p className="titleTopImg">
+            <br />
+            LE LIS
+            <br />
+            BLANC
+          </p>
+          <img src={img6} alt="imagem destaque 2" />
+        </div>
+
+      </div>
+    </>
   );
 
   return (
