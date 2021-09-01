627a8004f7ca67888ceca9a7292f8d7ce4210026
Adão Benites <adaobjr@gmail.com>
2021-09-01 10:26:04 -0400

    feat: :fire: removed routes not used in App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import { Switch, Route } from 'react-router-dom';
 import Home from './common/pages/Home';
-// import Favorited from './common/pages/Favorited';
-import Cart from './common/pages/Cart';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
-      {/* <Route exact path="/favoritos" component={Favorited} /> */}
-      <Route exact path="/carrinho" component={Cart} />
     </Switch>
   );
 }
