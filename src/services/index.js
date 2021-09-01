598e0e24aabcec5b311d2425e24d8c74cd70424b
Adão Benites <adaobjr@gmail.com>
2021-08-31 22:39:49 -0400

    feat: ✨ updated mock files

--- src/services/index.js
+++ src/services/index.js
@@ -8,11 +8,11 @@ import img4 from '../files/images/image_4.png';
 const clothes = [
   {
     id: 1,
-    title: 'MACACÃO FEMININO MARIA FILO SOBREPOSIÇÃO',
+    title: 'BLAZER FEMININO LIOCEL PUNHO',
     price: 759.00,
     parcel: '12 X R$ 63,25',
-    msg: 'VERÃO 2022',
-    image: img1,
+    msg: 'LANÇAMENTO',
+    image: img4,
     favorited: false,
   },
   {
@@ -35,11 +35,11 @@ const clothes = [
   },
   {
     id: 4,
-    title: 'BLAZER FEMININO LIOCEL PUNHO',
+    title: 'MACACÃO FEMININO MARIA FILO SOBREPOSIÇÃO',
     price: 759.00,
     parcel: '12 X R$ 63,25',
-    msg: 'LANÇAMENTO',
-    image: img4,
+    msg: 'VERÃO 2022',
+    image: img1,
     favorited: false,
   },
   {
@@ -53,15 +53,6 @@ const clothes = [
   },
   {
     id: 6,
-    title: 'REGATA FEMININA DE TRICOT COM OMBREIRA',
-    price: 759.00,
-    parcel: '12 X R$ 63,25',
-    msg: '20% OFF',
-    image: img2,
-    favorited: false,
-  },
-  {
-    id: 7,
     title: 'BLUSA FEMININA SILK MERCI LIOCEL',
     price: 759.00,
     parcel: '12 X R$ 63,25',
@@ -70,12 +61,21 @@ const clothes = [
     favorited: false,
   },
   {
-    id: 8,
-    title: 'BLAZER FEMININO LIOCEL PUNHO',
+    id: 7,
+    title: 'REGATA FEMININA DE TRICOT COM OMBREIRA',
     price: 759.00,
     parcel: '12 X R$ 63,25',
-    msg: 'LANÇAMENTO',
-    image: img4,
+    msg: '20% OFF',
+    image: img2,
+    favorited: false,
+  },
+  {
+    id: 8,
+    title: 'BLUSA FEMININA SILK MERCI LIOCEL',
+    price: 759.00,
+    parcel: '12 X R$ 63,25',
+    msg: '',
+    image: img3,
     favorited: false,
   },
   {
@@ -98,11 +98,11 @@ const clothes = [
   },
   {
     id: 11,
-    title: 'BLUSA FEMININA SILK MERCI LIOCEL',
+    title: 'BLAZER FEMININO LIOCEL PUNHO',
     price: 759.00,
     parcel: '12 X R$ 63,25',
-    msg: '',
-    image: img3,
+    msg: 'LANÇAMENTO',
+    image: img4,
     favorited: false,
   },
   {
